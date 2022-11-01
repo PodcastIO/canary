9f0765b3c2573a3285e3758ce4588dcf4486613e
mrbean <mrbean>
2022-11-01 23:03:04 +0800

    deploy-zh set tmp directory

--- docker/deploy-zh.sh
+++ docker/deploy-zh.sh
@@ -6,7 +6,7 @@ STAT_JOBS=("en" 0 "zh" 0 "ja" 0 "de" 0 "nl" 0 "fr" 0 "es" 0)
 CUR_DIR=$(cd `dirname $0`; pwd)
 
 UNIX_TIME=$(date +%s)
-DOCKER_COMPOSE_FILE="${CUR_DIR}/docker-compose_${UNIX_TIME}.yml"
+DOCKER_COMPOSE_FILE="/tmp/docker-compose_${UNIX_TIME}.yml"
 
 
 ARGS=${@}
